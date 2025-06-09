0849a970a886506ca86a05aa4fbc10a3dfe2fc71
qqaazz800624 <qqaazz800624@gmail.com>
2025-06-09 13:25:02 +0800

    updates

--- scripts/test.py
+++ scripts/test.py
@@ -1,38 +1,15 @@
 #%%
 
-import numpy as np
-import math
+def hash(k):
+    return k % 7
 
-def D(t):
-    return math.ceil(math.log(4097, 2*t))
 
-print(D(8))
-print(D(9))
-print(D(10))
-print(D(14))
-print(D(33))
+hash(129)
+
 
 #%%
 
-def block(t):
-    return (1+0.032*t)
 
-print(block(8))
-print(block(9))
-print(block(10))
-print(block(14))
-print(block(33))
-
-#%%
-
-def T(t):
-    return D(t) * block(t)
-
-print(T(8))
-print(T(9))
-print(T(10))
-print(T(14))
-print(T(33))
 
 
 #%%
